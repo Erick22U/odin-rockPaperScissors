1ba02603b5591dbe99a8f9dc3ecc0cf64742e641
Erick Uresti <erick22uresti@gmail.com>
2023-02-10 13:59:03 -0600

    made it so where clicking a button updates the choices on screen

--- game.js
+++ game.js
@@ -1,4 +1,10 @@
-const getComputerChoice = () => {
+//Scores for the player and computer
+let playerScore = 0;
+let computerScore = 0;
+let roundWinnner = "";
+
+//get the choice for the computer
+function getComputerChoice() {
   let number = Math.floor(Math.random() * 3);
 
   switch (number) {
@@ -9,24 +15,83 @@ const getComputerChoice = () => {
     case 2:
       return "Paper";
   }
-};
-
-
-
-const getPlayerChoice = (button) => {
-    const id = button.getAttribute('id');
-    if(id === "rockBtn") return "Rock";
-    else if(id === "paperBtn") return "Paper";
-    else return "Scissors";
 }
 
-const buttons = document.querySelectorAll('.btn')
+//Play game
+function playRound(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) return "tie";
 
+  if (
+    (playerChoice == "Rock" && computerChoice == "Paper") ||
+    (playerChoice == "Paper" && computerChoice == "Scissors") ||
+    (playerChoice == "Scissors" && computerChoice == "Rock")
+  ) {
+    computerScore++;
+    roundWinnner = "computer";
+  } else {
+    playerScore++;
+    roundWinnner = "player";
+  }
+}
 
+//is game over
+function isGameOver() {
+  if (computerScore == 5 || playerScore == 5) {
+    console.log("Game over");
+    return true;
+  } else return false;
+}
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        const playerChoice = getPlayerChoice(button);
-        console.log(playerChoice)
-    })
-});
+//updates the choice in the gui
+function updateChoiceSelection(playerChoice, computerChoice) {
+  switch (playerChoice) {
+    case "Rock":
+      guiPlayerChoice.innerText = btnSign[0].textContent;
+      break;
+    case "Paper":
+      guiPlayerChoice.innerText = btnSign[1].textContent;
+      break;
+    case "Scissors":
+      guiPlayerChoice.innerText = btnSign[2].textContent;
+      break;
+  }
+
+  switch (computerChoice) {
+    case "Rock":
+      guiComputerChoice.innerText = btnSign[0].textContent;
+      break;
+    case "Paper":
+      guiComputerChoice.innerText = btnSign[1].textContent;
+      break;
+    case "Scissors":
+      guiComputerChoice.innerText = btnSign[2].textContent;
+      break;
+  }
+}
+
+// Starts the game and decides winner
+function onClick(playerChoice) {
+  isGameOver();
+
+  const computerChoice = getComputerChoice();
+  playRound(playerChoice, computerChoice);
+
+  updateChoiceSelection(playerChoice, computerChoice);
+}
+
+// Get all the buttons
+const rockBtn = document.getElementById("rockBtn");
+const paperBtn = document.getElementById("paperBtn");
+const scissorsBtn = document.getElementById("scissorsBtn");
+
+//get text where the choices is placed
+const guiPlayerChoice = document.getElementById("playerChoice");
+const guiComputerChoice = document.getElementById("computerChoice");
+
+//button signs
+const btnSign = document.getElementsByClassName("sign");
+
+//Adding events to all buttons
+rockBtn.addEventListener("click", () => onClick("Rock"));
+paperBtn.addEventListener("click", () => onClick("Paper"));
+scissorsBtn.addEventListener("click", () => onClick("Scissors"));
